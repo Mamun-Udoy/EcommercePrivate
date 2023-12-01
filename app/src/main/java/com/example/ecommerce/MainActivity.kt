5af71bd1a0bf34d85104390e1383864e4f1a3621
Mamun-Udoy <abdullah15-2696@diu.edu.bd>
2023-12-01 18:27:44 +0600

    Add Full Screen System

--- app/src/main/java/com/example/ecommerce/MainActivity.kt
+++ app/src/main/java/com/example/ecommerce/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import android.view.WindowManager
 import androidx.activity.viewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
@@ -29,7 +30,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
